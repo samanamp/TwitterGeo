c3df3367a4d92a8459a7d16c277e066533b480c2
Cesar Martinez D Granda <checharo@gmail.com>
2013-10-07 15:30:22 +1100

    Finished languages.js partly, and counting.js completely

--- js/test.js
+++ js/test.js
@@ -1,26 +1,30 @@
 
+/* This should be set to the reverse proxy value of the couchdb in the Apache configuration */
 var server = 'http://localhost/couchdb/';
 
 function onload() {
 	
-	var res = getLanguageTweetsPerDay(2013, 9, 30);
+	var res = getSentimentTweets();
 	var text = '';
 	for (var i = 0; i < res.length; i++) {
 		text += res[i][0] + ' - ' + res[i][1] + '<br/>';
 	}
-	$('#results').html(text);
+	//$('#results').html(text);
 	
-	alert(JSON.stringify(samanTest()));
+	$('#results').html(JSON.stringify(getD3JSONSentimentOn(2013,9,30)));
 	
 };
 
-function getLanguageTweetsPerDay(year, month, day) {
+/* Returns in a bi-dimensional array the number of tweets per language. The
+ * first element is the two digit code of the language, and the second the
+ * number of tweets e.g. res[0][0] = 'ar' and res[0][1] = 500.
+ */
+function getLanguageTweets() {
 	var json = '';
-	month--; //JavaScript Date works with months 0 - 11
 	$.ajax({
 		  type: 'GET',
 		  dataType: 'json',
-		  url: server + 'twittering_replica/_design/language/_view/per_day?group_level=4',
+		  url: server + 'twittering_replica/_design/language/_view/per_day?group_level=1',
 		  success: function(result) {
 			  json = result;
 		  },
@@ -28,46 +32,47 @@ function getLanguageTweetsPerDay(year, month, day) {
 	});
 	
 	var res = new Array();
-	for (var i = 0, j = 0; i < json.rows.length; i++) {
+	for (var i = 0; i < json.rows.length; i++) {
 		var k = json.rows[i].key;
 		var v = json.rows[i].value;
-		if (k[1] != year || k[2] != month || k[3] != day) 
-			continue;
-		res[j] = new Array(2);
-		res[j][0] = k[0];
-		res[j][1] = v;
-		j++;
+		res[i] = new Array(2);
+		res[i][0] = k[0];
+		res[i][1] = v;
 	}
 	
 	return res;
 };
 
-function samanTest() {
-	
-	var doc = '';
+/* Returns in a bi-dimensional array the number of tweets per language, given
+ * a specified day by the input arguments. 
+ * The first element is the two digit code of the language, and the second the
+ * number of tweets e.g. res[0][0] = 'ar' and res[0][1] = 500.
+ */
+function getLanguageTweetsPerDay(year, month, day) {
+	var json = '';
+	month--;
 	$.ajax({
 		  type: 'GET',
 		  dataType: 'json',
-		  url: server + 'twittering_replica/_design/language/_view/coordinates',
+		  url: server + 'twittering_replica/_design/language/_view/per_day_range?'
+		  	+ 'group_level=4&startkey=[' + year + ',' + month + ',' + day + ']&endkey=[' 
+		  	+ year + ',' + month + ',' + day + 1 + ']',
 		  success: function(result) {
-			  doc = result;
+			  json = result;
 		  },
 		  async: false
 	});
 	
-	var res = {
-		"extent" : [150.821457,-34.026391,151.319962,-33.724244],
-		"features" : []
-	};
-	for (var i = 0; i < doc.total_rows; i++) {
-		var point = {
-			"geometry": {
-				"type" : "point",
-				"coordinates" : [doc.rows[i].value[1],doc.rows[i].value[2]]
-			}
-		}
-		res.features.push(point);
+	var res = new Array();
+	for (var i = 0; i < json.rows.length; i++) {
+		var k = json.rows[i].key;
+		var v = json.rows[i].value;
+		res[i] = new Array(2);
+		res[i][0] = k[0];
+		res[i][1] = v;
 	}
+	
 	return res;
-}
+};
+
 
